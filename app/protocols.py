e223854d2f72513c84c708dad322fb3121c8febc
Andrei Chenchik <andrei@chenchik.me>
2022-07-06 07:56:34 +0200

    Add encryption protocol

--- app/protocols.py
+++ app/protocols.py
@@ -4,3 +4,11 @@ from typing import Protocol
 class Infrastructure(Protocol):
     def update(self) -> bool:
         ...
+
+
+class Encryption(Protocol):
+    def encrypt(self, string: str) -> bytes:
+        ...
+
+    def decrypt(self, token: bytes) -> str:
+        ...
