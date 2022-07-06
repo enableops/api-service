fe476e4d93562fe40a2e118225bd05a68a5933e5
Andrei Chenchik <andrei@chenchik.me>
2022-07-06 16:11:04 +0200

    Add dependencies implementation

---
+++ app/dependencies.py
@@ -0,0 +1,51 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from app import settings
+from app import main
+from app import oauth
+from services import googleauth, jwt
+
+from app.database.crud import user as crud
+from app.models.token import TokenData
+from app.models.user import User
+
+
+async def get_db():
+    db = main.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+get_oauth = lambda: googleauth.get_gauth()
+get_token_service = lambda: jwt.get_jwt()
+get_app_settings = lambda: settings.Settings().api
+
+
+def get_current_user(
+    token: str = Depends(oauth.oauth2_scheme),
+    db: Session = Depends(get_db),
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_service = jwt.get_jwt()
+    try:
+        payload = token_service.decode(jwt_token=token)
+        uid: str = payload["sub"]
+        if uid is None:
+            raise credentials_exception
+        token_data = TokenData(sub=uid)
+        uid = token_data.sub
+    except:
+        raise credentials_exception
+
+    db_user = crud.get_user(db=db, uid=uid)
+    if db_user is None:
+        raise credentials_exception
+
+    return User.from_orm(db_user)
