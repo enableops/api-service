bebd854bf48accc5620f86466b415f87e441fb93
Andrei Chenchik <andrei@chenchik.me>
2022-07-06 13:20:47 +0200

    update crypto service

--- services/crypto.py
+++ services/crypto.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import secrets
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
@@ -13,23 +14,30 @@ class Settings(BaseModel):
 
     class Config:
         env_prefix = "CRYPTO_"
+        secrets_dir = os.getenv("SECRETS_PATH")
 
 
 class Crypto:
     key: bytes
 
     def __init__(self, *, settings: Settings):
-        if self.key == default_key:
-            logging.exception("Using default key for data encryption")
-
         self.key = settings.key
 
-    def encrypt(self, string: str) -> bytes:
+    def encrypt(self, *, string: str) -> bytes:
         nonce = secrets.token_bytes(12)
         payload = string.encode()
         token = nonce + AESGCM(self.key).encrypt(nonce, payload, b"")
         return token
 
-    def decrypt(self, token: bytes) -> str:
+    def decrypt(self, *, token: bytes) -> str:
         payload = AESGCM(self.key).decrypt(token[:12], token[12:], b"")
         return payload.decode()
+
+
+def get_crypto():
+    settings = Settings()
+
+    if settings.key == default_key:
+        logging.exception("Using default key for data encryption")
+
+    return Crypto(settings=settings)
