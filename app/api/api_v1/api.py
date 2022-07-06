30b5fca713b46d228cbdcf28aabfef2aec4bd3dd
Andrei Chenchik <andrei@chenchik.me>
2022-07-06 16:11:50 +0200

    turn off everything except auth

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import auth, user, customers
+from app.api.api_v1.endpoints import auth  # , user  # , customers
 from app import urls
 
 api_router = APIRouter()
@@ -9,14 +9,14 @@ api_router.include_router(
     router=auth.router, prefix=urls.Sections.auth, tags=["auth"]
 )
 
-api_router.include_router(
-    router=user.router,
-    prefix="/user",
-    tags=["user"],
-)
+# api_router.include_router(
+#     router=user.router,
+#     prefix="/user",
+#     tags=["user"],
+# )
 
-api_router.include_router(
-    router=customers.router,
-    prefix="/customers",
-    tags=["customers"],
-)
+# api_router.include_router(
+#     router=customers.router,
+#     prefix="/customers",
+#     tags=["customers"],
+# )
