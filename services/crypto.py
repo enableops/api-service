452b3aa99450315053efe88f62cb83809c1fc25c
Andrei Chenchik <andrei@chenchik.me>
2022-07-06 08:20:46 +0200

    update crypto service with default key

--- services/crypto.py
+++ services/crypto.py
@@ -1,15 +1,15 @@
+import logging
 import secrets
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
-from app.core.config import settings
 
-generate_random_key = lambda: secrets.token_bytes(32)
+default_key = b"pls help me god!"
 
 
 class Settings(BaseModel):
-    key: bytes = Field(default_factory=generate_random_key)
+    key: bytes = default_key
 
     class Config:
         env_prefix = "CRYPTO_"
@@ -19,6 +19,9 @@ class Crypto:
     key: bytes
 
     def __init__(self, *, settings: Settings):
+        if self.key == default_key:
+            logging.exception("Using default key for data encryption")
+
         self.key = settings.key
 
     def encrypt(self, string: str) -> bytes:
