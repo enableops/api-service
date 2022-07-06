1ad2eb8354741e3c6b02213aed612898cf4d5582
Andrei Chenchik <andrei@chenchik.me>
2022-07-06 16:08:20 +0200

    Add Token & TokenService protocols

--- app/protocols.py
+++ app/protocols.py
@@ -14,19 +14,35 @@ class Encryption(Protocol):
         ...
 
 
+class OAuthUser(Protocol):
+    uid: str
+    email: str
+    credentials: str
+
+
 class OAuth(Protocol):
+    client_id: str
+    prompt_type: str
+    access_type: str
+    scopes: str
+
     def get_auth_url_with_state(
         self, *, redirect_uri: str, state: Optional[str]
     ) -> Tuple[str, str]:
         ...
 
     def get_user_from_authcode(
-        self, *, code: str, redirect_url: str, state: str
-    ):
+        self, *, code: str, redirect_uri: str, state: str
+    ) -> OAuthUser:
         ...
 
 
-class OAuthUser(Protocol):
-    uid: str
-    email: str
-    credentials: str
+class Token(Protocol):
+    access_token: str
+    csrf_token: str
+    token_type: str
+
+
+class TokenService(Protocol):
+    def create_access_token(self, *, for_sub: str) -> Token:
+        ...
